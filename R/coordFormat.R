b0963987470305039de407cb3f1d9a3b9ed4eae0
manifest <robert.qiao@flinders.edu.au>
2019-12-28 23:52:32 +1030

    initial R package

---
+++ R/coordFormat.R
@@ -0,0 +1,22 @@
+# coordFormat
+#' Format input strings into GRange object
+#'
+#' Split input string with deliminator ":" then transform into GRange object
+#'
+#' @param mycoords.list a string list with the format of "2:16e7:16.5e7"
+#'
+#' @export
+#' @return a target search range as a GRanges object
+#' @examples
+#' coordFormat("2:16e7:16.5e7")
+
+coordFormat <- function (mycoords.list) {
+  GRangeObj <- lapply(mycoords.list, function (x) {res=strsplit(x, ':')}) %>% unlist  %>%
+                    as.numeric %>%
+                    matrix(ncol=3, byrow=T) %>%
+                    as.data.frame %>%
+                    dplyr::select(chrom=V1, start=V2, end=V3) %>%
+                    dplyr::mutate(chrom=paste0('chr', chrom)) %>%
+                    GenomicRanges::makeGRangesFromDataFrame()
+  return (GRangeObj)
+}
