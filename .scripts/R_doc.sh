e437afed2714d51e90c879ee9fbe95df77cd7afa
manifest <robert.qiao@flinders.edu.au>
2019-12-31 16:36:46 +1030

    .travis doc update

---
+++ .scripts/R_doc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+setup_git() {
+  git config --global user.email "qiao0015@flinders.edu.au"
+  git config --global user.name  "robqbot"
+}
+
+commit_R_docs() {
+  git checkout -b develop
+  git add DESCRIPTION	# commit new DESCRIPTION
+  git add NAMESPACE     # commit new NAMESPACE
+  git add man 		# commit manual 
+  git commit --message "robqbot travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_R_docs() {
+  git remote add origin-SynViz https://${robqbot_TOKEN}@github.com/DPP4ResearchGroup/SyntenyViz.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-SynViz develop:develop  
+}
+
+setup_git
+commit_R_docs
+upload_R_docs
+
