633502389b25eb2d02e4cf47ceb5a466d41d6761
manifest <robert.qiao@flinders.edu.au>
2020-01-23 13:49:05 +1030

    init update

--- data-raw/orgOrgDBTable.R
+++ data-raw/orgOrgDBTable.R
@@ -1,5 +1,5 @@
 ## Read orgmTable.csv to form orgmOrgDB data.frame
 library(usethis)
 
-orgmOrgDB <- read.csv("data-raw/orgDBTable.csv", stringsAsFactors = FALSE)
+orgmOrgDB <- read.csv("data-raw/orgOrgDBTable.csv", stringsAsFactors = FALSE)
 usethis::use_data(orgmOrgDB, overwrite=T)
