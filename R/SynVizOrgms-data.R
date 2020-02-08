4708fb074f882020f8a4f72398b2678bf45ee0ea
manifest <robert.qiao@flinders.edu.au>
2020-02-08 10:46:32 +1030

    vignettes data

---
+++ R/SynVizOrgms-data.R
@@ -0,0 +1,23 @@
+#' A data table that is used to store available organisms for analysis
+#'
+#' SynVizOrgms is a data.frame that stores available organisms for analysis
+#'
+#' @docType data
+#'
+#' @usage data(SynVizOrgms)
+#'
+#' @format A data.frame with tags: "Scientific.Names", "Common.Names", "Abbrav", "Abbrav.Short"
+#' @keywords datasets
+#'
+#' @source
+#'
+#' @family SyntenyViz
+#'
+#' @examples
+#' data(SynVizOrgms)
+#' SciName <- SynVizOrgms$Scientific.Names
+#' ComName <- SynVizOrgms$Common.Names
+#' Abbrav <- SynVizOrgms$Abbrav
+#' ShortAbbrav <- SynVizOrgms$Abbrav.Short
+
+"SynVizOrgms"
