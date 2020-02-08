16a7b9506c3db8edf07df11014981d88c2427963
manifest <robert.qiao@flinders.edu.au>
2020-02-08 11:34:24 +1030

    vig pub +

--- .scripts/R_doc.sh
+++ .scripts/R_doc.sh
@@ -12,13 +12,16 @@ source .scripts/R_publish_framework.sh --source-only
 
 commit_R_docs() {
   git checkout develop
-# git add DESCRIPTION	# commit new DESCRIPTION
   git add NAMESPACE     # commit new NAMESPACE
   git add man 		# commit manual 
   git commit --m "[skip travis] documentation @robqbot travis build: $TRAVIS_BUILD_NUMBER"
 }
 
+commit_R_vignettes() {
+  ls -aRl 
+}
+
 setup_git
 commit_R_docs
+commit_R_vignettes
 upload_R_docs
-
