0f7e37aaa4a23fad2270286bce5c8ae530a4b707
manifest <robert.qiao@flinders.edu.au>
2020-02-08 10:47:28 +1030

    vignettes data

--- data-raw/orgOrgDBTable.R
+++ data-raw/orgOrgDBTable.R
@@ -1,5 +1,5 @@
 ## Read orgmTable.csv to form orgmOrgDB data.frame
 library(usethis)
 
-orgmOrgDB <- read.csv("data-raw/orgOrgDBTable.csv", stringsAsFactors = FALSE)
+orgmOrgDB <- read.csv("data-raw/orgOrgDBTable.csv", header=TRUE, stringsAsFactors = FALSE)
 usethis::use_data(orgmOrgDB, overwrite=T)
