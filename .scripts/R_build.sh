e7ee5cc768771737ad3343a39c80f6e9a45114fd
manifest <robert.qiao@flinders.edu.au>
2020-02-10 16:57:55 +1030

    .travis multibuild debug

---
+++ .scripts/R_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Include R CMD build and R CMD check
+R_build_docs() {
+  R -e 'devtools::document()'
+  R -e 'devtools::build_vignettes()'
+}
+
+R_build_pkgs() {
+  cd $TRAVIS_BUILD_DIR/../
+  R CMD build $packageName
+  R CMD check ${packageName}_*.tar.gz --check-subdirs=yes
+}
+
+R_build_docs
+R_build_pkgs
