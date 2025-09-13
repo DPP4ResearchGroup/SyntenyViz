acad49d28a36865d8f242a9f34cb7229125a8765
robqbot <31822773+robqbot@users.noreply.github.com>
2025-09-13 14:40:53 +0000

    [skip ci] documentation @robqbot github actions build: 77

---
+++ vignettes/calculateOrthologSimilarity_Explanatory.R
@@ -0,0 +1,9 @@
+## ----setup, include=FALSE-----------------------------------------------------
+knitr::opts_chunk$set(
+  echo = TRUE,
+  warning = FALSE,
+  message = FALSE,
+  fig.width = 8,
+  fig.height = 6
+)
+
